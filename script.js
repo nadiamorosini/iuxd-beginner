092874b1bd8ffe8fb0290fad5bbbd00810029429
JackMeerkerk <56542504+JackMeerkerk@users.noreply.github.com>
2020-01-09 12:13:36 +0100

    werkende nav bar

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    setBindings();
+});
+
+function setBindings() {
+    $("li a").click(function(e){
+        e.preventDefault();
+        let sectionID = e.currentTarget.id + "Section";
+
+        $("html, body").animate({
+             scrollTop: $("#" + sectionID).offset().top
+        }, 1000)
+    })
+}
